de7460354f38fe0386aedc2bd6c8eccf1bec8466
ENAIKA <naikaesther5@gmail.com>
2020-06-10 19:26:01 +0300

    add urls for login,logout,registrartion,uploadproject, allprojects, project

--- points/views.py
+++ points/views.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
-from django.shortcuts import render,redirect
 from .email import send_welcome_email
 import datetime as dt
 from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.shortcuts import render,redirect,get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import  Project,UserProfile,NewsLetterRecipients,Rates
@@ -54,7 +54,7 @@ class UserDescription(APIView):
     def get_user(self, pk):
         try:
             return UserProfile.objects.get(pk=pk)
-        except UserProfile.DoesNotExist:
+        except DoesNotExist:
             return Http404
 
     def get(self, request, pk, format=None):
@@ -89,7 +89,7 @@ class ProjectDescription(APIView):
     def get_project(self, pk):
         try:
             return Project.objects.get(pk=pk)
-        except Project.DoesNotExist:
+        except DoesNotExist:
             return Http404
 
     def get(self, request, pk, format=None):
@@ -138,6 +138,7 @@ def registerPage(request):
         form=SignUpForm()
     
     return render(request, 'django_registration/registration_form.html', {'form':form})
+
 def login_view(request):
     
     if request.method=='POST':
@@ -149,7 +150,7 @@ def login_view(request):
 
             login(request, user)
 
-            return redirect('allphotos')
+            return redirect('allprojects')
         else:
             return HttpResponse("invalid login credentials")
     
@@ -159,7 +160,7 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
-
+@login_required 
 def search_results(request):
 
     if 'project' in request.GET and request.GET["project"]:
@@ -172,22 +173,47 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'all/search.html',{"message":message})
+
+
+@login_required  
 def increment_counter(request,project_id):
     if not request.user.is_authenticated:
         return redirect('django_registration/login.html')
     else:
         if request.method=="POST":
             if 'like' in request.POST:
-                liked = get_object_or_404(Image, id=request.POST.get('project_id'))
-                liked.likes.add(request.user)
+                liked = get_object_or_404(Project, id=request.POST.get('project_id'))
+                liked.like.add(request.user)
                 
             return HttpResponseRedirect(reverse("liked", args=[str(project_id)]) )#check this too
+
+
+@login_required  
 def project(request,project_id):
     try:
         project = Project.objects.get(id =project_id)
-    except ObjectDoesNotExist:
+    except DoesNotExist:
         raise Http404()
     return render(request,"all/project.html", {"project":project})
+
+@login_required  
 def allprojects(request):
     projects=Project.objects.all()
     return render(request, 'allprojects.html', {"projects":projects})
+
+@login_required
+def uploadproject(request,user_id):
+    user=User.objects.get(pk=user_id)
+    if request.method=="post":
+        form=ProjectForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            project_name = form.cleaned_data['project_name']
+            description = form.cleaned_data['description']
+            url = form.cleaned_data['url']
+            image= form.cleaned_data['image']
+            project=Project(project_name=project_name,url=url, description=description,image=image,user=user)
+            project.save()
+            HttpResponseRedirect('allprojects.html')
+    else:
+        form = ProjectForm()
+    return render(request, 'uploadproject.html', {"form":form})
