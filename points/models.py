06b0316ac850724f2b06edfaf8729548f41beef2
ENAIKA <naikaesther5@gmail.com>
2020-06-10 19:21:14 +0300

    add like field, and created and posted date fields, deleted AllProjects and AllUsers models

--- points/models.py
+++ points/models.py
@@ -1,3 +1,59 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
+import datetime
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save 
+from django.dispatch import receiver
+from django.urls import reverse
+from django.core.validators import RegexValidator
 
 # Create your models here.
+class UserProfile(models.Model):
+    photo=models.ImageField(upload_to = 'profile/', blank=True)
+    bio=models.TextField()
+    email=models.EmailField()
+    user=models.OneToOneField(User, on_delete=models.CASCADE,related_name="userprofile")
+    created=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User) 
+def create_user_profile(sender, instance, created, **kwargs):
+     if created:
+         UserProfile.objects.create(user=instance)
+@receiver(post_save, sender=User) 
+def save_user_profile(sender, instance, **kwargs):
+     instance.userprofile.save()
+
+
+
+class Project(models.Model):
+    author=models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    url=models.URLField(max_length = 200)
+    description=models.TextField()
+    like=models.ManyToManyField(settings.AUTH_USER_MODEL,blank=True,related_name="liked" )
+    project_name=models.CharField(max_length=150)
+    image=models.ImageField(upload_to = 'projects/', blank=True)
+    posted=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.project_name
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        projects = cls.objects.filter(name__icontains=search_term)
+        return projects
+
+
+class Rates(models.Model):
+    jury=models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    design=models.IntegerField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')])
+    content=models.IntegerField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')])
+    usability=models.IntegerField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')])
+
+
+class NewsLetterRecipients(models.Model):
+    name=models.CharField(max_length=40) 
+    email= models.EmailField()
