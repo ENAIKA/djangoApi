967c5fd40ad5b5438c9b3b006a91addc1d575c40
ENAIKA <naikaesther5@gmail.com>
2020-06-11 07:15:39 +0300

    register models on admin

--- points/admin.py
+++ points/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Project,UserProfile,Rates,NewsLetterRecipients
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(UserProfile)
+admin.site.register(Rates)
+admin.site.register(NewsLetterRecipients)
