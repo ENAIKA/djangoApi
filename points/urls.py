abbb87d836bcecc9150284aab973404be34c690b
ENAIKA <naikaesther5@gmail.com>
2020-06-11 07:10:27 +0300

    update urlpattern for view functions

--- points/urls.py
+++ points/urls.py
@@ -1,8 +1,9 @@
-from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views
-from django.urls import path
+from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     url(r'^$',views.welcome,name = "welcome"),
@@ -12,13 +13,18 @@ urlpatterns=[
     url(r'api/project/project-id/(?P<pk>[0-9]+)/$',views.ProjectDescription.as_view()),
     url(r'^ajax/welcomeletter/$', views.welcomeletter, name='welcomeletter'),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'updateprofile/(?P<user_id>[0-9]+)',views.updateprofile,name='updateprofile'),
     path('register/', views.registerPage, name="django_registration_register"),
     url(r'login/',views.login_view,name="login"), 
     url(r'^project/(\d+)',views.project,name ='project'),
     url(r'^projects/',views.allprojects,name ='allprojects'),
     path('logout/', views.logout_view, name="logout"),
+    url(r'juryverdict/(?P<project_id>[0-9]+)', views.juryverdict, name="juryverdict"),
+    url(r'^myprojects/(?P<user_id>[0-9]+)',views.userprojects,name ='myprojects'),
+    url(r'^userprofile/(?P<user_id>[0-9]+)',views.userprofile,name ='userprofile'),
     url(r'project/(?P<user_id>[0-9]+)/like', views.increment_counter, name='liked'),
     url(r'^uploadproject/(\d+)',views.uploadproject,name ='uploadpproject'),
+    path('change-password/',auth_views.PasswordChangeView.as_view(template_name='change-password.html'),name='password_reset'), 
 
 ]
  
